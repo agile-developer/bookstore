19d72ceaaae500396d745151d70084050ac3a010
Faisal Khan <faisalalik@outlook.com>
2024-08-14 11:36:40 +0100

    Synchronize on 'CreateOrderRequest' instance to avoid race condition where two threads try to create an order for the same quote

--- app/src/main/kotlin/co/bound/exercise/bookstore/order/service/OrderServiceImpl.kt
+++ app/src/main/kotlin/co/bound/exercise/bookstore/order/service/OrderServiceImpl.kt
@@ -19,33 +19,35 @@ class OrderServiceImpl(
     private val createdOrders = ConcurrentHashMap<String, Order>()
 
     override fun createOrder(request: CreateOrderRequest): CreateOrderResult {
-        if (createdOrders.containsKey(request.idempotencyId)) {
-            logger.info("IdempotencyId: ${request.idempotencyId} already known, returning existing order")
-            return CreateOrderResult.Success(createdOrders[request.idempotencyId]!!)
-        }
+        synchronized(request) {
+            if (createdOrders.containsKey(request.idempotencyId)) {
+                logger.info("IdempotencyId: ${request.idempotencyId} already known, returning existing order")
+                return CreateOrderResult.Success(createdOrders[request.idempotencyId]!!)
+            }
 
-        val bookQuote = quoteService.getQuote(request.quoteId)
-        if (bookQuote == null) {
-            logger.error("QuoteId: ${request.quoteId} is not valid")
-            return CreateOrderResult.Error(request.quoteId)
-        }
+            val bookQuote = quoteService.getQuote(request.quoteId)
+            if (bookQuote == null) {
+                logger.error("QuoteId: ${request.quoteId} is not valid")
+                return CreateOrderResult.Error(request.quoteId)
+            }
 
-        logger.info("Calling Valdivia to purchase quoteId: ${request.quoteId}")
-        runCatching {
-            valdiviaClient.purchaseBook(request.quoteId)
-        }.getOrElse {
-            logger.error("Error creating order for quoteId: ${request.quoteId}, message: ${it.message}")
-            return CreateOrderResult.Error(request.quoteId)
-        }
+            logger.info("Calling Valdivia to purchase quoteId: ${request.quoteId}")
+            runCatching {
+                valdiviaClient.purchaseBook(request.quoteId)
+            }.getOrElse {
+                logger.error("Error creating order for quoteId: ${request.quoteId}, message: ${it.message}")
+                return CreateOrderResult.Error(request.quoteId)
+            }
 
-        val order = Order(
-            idempotencyId = request.idempotencyId,
-            bookQuote = bookQuote,
-            status = Order.Status.CREATED,
-            deliveryAddress = request.deliveryAddress
-        )
-        createdOrders[request.idempotencyId] = order
-        quoteService.invalidateQuote(request.quoteId)
-        return CreateOrderResult.Success(order)
+            val order = Order(
+                idempotencyId = request.idempotencyId,
+                bookQuote = bookQuote,
+                status = Order.Status.CREATED,
+                deliveryAddress = request.deliveryAddress
+            )
+            createdOrders[request.idempotencyId] = order
+            quoteService.invalidateQuote(request.quoteId)
+            return CreateOrderResult.Success(order)
+        }
     }
 }
